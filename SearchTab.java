8bba5cfde090a3c3ce4854b1ba54e23786004c94
Matthew Flautt <cablefast@gmail.com>
2018-11-10 19:39:31 -0500

    More searchtab work today.
    still trying to get jfilechooser to work with the export button.
    got the search function to work by asset serial number.
    got the clear button to work.

--- SearchTab.java
+++ SearchTab.java
@@ -1,19 +1,32 @@
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import net.miginfocom.swing.MigLayout;
-import javax.swing.JToggleButton;
+import javax.swing.JFileChooser.*;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.JTextArea;
 import javax.swing.JScrollPane;
 import javax.swing.ButtonGroup;
+import javax.swing.JToggleButton;
+import net.miginfocom.swing.MigLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.util.ArrayList;
 
 
-public class SearchTab extends JPanel{
+
+
+public class SearchTab extends JPanel implements ActionListener{
 
 	private static final long serialVersionUID = 1L;
 	private JTextField txtfldUserSearchParam;
+	private JTextArea txtaraSearchResults;
+	private JButton btnClear, btnExport;
+	private JFileChooser fc;
 	private final ButtonGroup buttonGroupSearchTypes = new ButtonGroup();
+	private static ArrayList<String> result=new ArrayList<String>();
 
 	public SearchTab() {
 		setLayout(new MigLayout("", "[100,grow][100,grow][100,grow][100,grow]", "[grow][][][]"));
@@ -22,11 +35,12 @@ public class SearchTab extends JPanel{
 		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
 		add(scrollPane, "cell 0 0 4 1,grow");
 		
-		JTextArea txtaraSearchResults = new JTextArea();
+		txtaraSearchResults = new JTextArea();
 		txtaraSearchResults.setEditable(false);
 		scrollPane.setViewportView(txtaraSearchResults);
 		
 		JToggleButton tglbtnByAssetTag = new JToggleButton("Asset Tag");
+		tglbtnByAssetTag.setSelected(true);
 		buttonGroupSearchTypes.add(tglbtnByAssetTag);
 		add(tglbtnByAssetTag, "cell 0 1,grow");
 		
@@ -43,19 +57,44 @@ public class SearchTab extends JPanel{
 		add(tglbtnAllAssets, "cell 3 1,grow");			
 		
 		txtfldUserSearchParam = new JTextField();
-		txtfldUserSearchParam.setText("Search");
+		txtfldUserSearchParam.setToolTipText("Search");
+		txtfldUserSearchParam.addActionListener(this);
 		add(txtfldUserSearchParam, "cell 1 2 2 1,grow");
 		txtfldUserSearchParam.setColumns(10);
 		
-		JButton btnClear = new JButton("Clear");
+		btnClear = new JButton("Clear");
+		btnClear.addActionListener(this);
 		add(btnClear, "cell 1 3,grow");
 		
-		JButton btnExport = new JButton("Export");
+		btnExport = new JButton("Export");
+		btnExport.addActionListener(this);
 		add(btnExport, "cell 2 3,grow");
-
-		
 	}
 	
-
-
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnExport) {
+			int returnVal = 1;//fc.showSaveDialog(SearchTab.this);
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				File file = fc.getSelectedFile();
+				txtaraSearchResults.append("Saving: " + file.getName() + "." + "\n");
+			} else {
+				txtaraSearchResults.append("Save command cancelled by user." + "\n");
+            }
+			txtaraSearchResults.setCaretPosition(txtaraSearchResults.getDocument().getLength());
+		
+		}
+		
+		
+		
+		else if (e.getSource() == btnClear) {
+			txtaraSearchResults.setText(null);
+		}
+		else if (e.getSource() == txtfldUserSearchParam) {
+			result = connectSQL.searchForAsset(txtfldUserSearchParam.getText());
+			for (int i = 0; i < result.size(); i++) {
+				txtaraSearchResults.append(result.get(i) + "\n");
+			}
+			result.clear();
+		}
+	}
 }
--- connectSQL.java
+++ connectSQL.java
@@ -1,19 +1,59 @@
 import java.sql.*;
+import java.io.StringWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class connectSQL {
+	private static String url = "jdbc:mysql://192.168.32.130:3306/br7";
+	private static String username = "mf56";
+	private static String password = "narrate^lips";
+	private static String row;
+	private static String sqlAssetSearch = "select * from asset where DSerNum = ?;";
+	private static ArrayList<String> result=new ArrayList<String>();
 	
-	
-	//public void connectToSQL() {
-	public static void main(String args[]){
-		String url = "jdbc:mysql://192.168.32.130:3306/br7";
-		String username = "mf56";
-		String password = "narrate^lips";
-		System.out.println("Connecting database...");
+	public static String connectToSQL() {
 		try (Connection connection = DriverManager.getConnection(url, username, password)) {
-		    System.out.println("Database connected!");
+			connection.close();
+			return "Database connected!";
 		} catch (SQLException e) {
-		    throw new IllegalStateException("Cannot connect the database!", e);
+			return "Cannot connect the database!";
 		}
+	}
+	
+	public static ArrayList<String> searchForAsset(String sernum) {
+		try (Connection con = DriverManager.getConnection(url, username, password)) {
+			if (sernum.isEmpty()) {
+				Statement stmt=con.createStatement();
+				ResultSet rs=stmt.executeQuery("select * from asset");
+				while(rs.next()) {
+					row = rs.getString(1)+",  "+rs.getString(2)+",  "+rs.getString(3)+",  "+rs.getString(4)+",  "+rs.getString(5)+",  "+rs.getString(6)+",  "+rs.getString(7);
+					result.add(row);
+				}
+				stmt.close();
+				rs.close();
+			}
+			else {
+				PreparedStatement pstmt = con.prepareStatement(sqlAssetSearch);
+				pstmt.setString(1, sernum);
+				ResultSet rs=pstmt.executeQuery();
+				while(rs.next()) {
+					row = rs.getString(1)+"  "+rs.getString(2)+"  "+rs.getString(3)+"  "+rs.getString(4)+"  "+rs.getString(5)+"  "+rs.getString(6)+"  "+rs.getString(7);
+					result.add(row);
+				}
+				pstmt.close();
+				rs.close();
+			}
+			con.close();
+			return result;
+		} catch (SQLException e) {
+			StringWriter sw = new StringWriter();
+			PrintWriter pw = new PrintWriter(sw);
+			e.printStackTrace(pw);
+			row = sw.toString();
+			result.add(row);
+			return result;
+		}
+
 	}
 
 }
