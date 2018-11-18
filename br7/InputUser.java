7bc5914c968f3001b5ef53c85cfddc5eada1b327
Matthew Flautt <cablefast@gmail.com>
2018-11-18 13:28:59 -0500

    New input user form - blank

---
+++ br7/InputUser.java
@@ -0,0 +1,26 @@
+package br7;
+
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+
+public class InputUser extends JFrame implements ActionListener {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public InputUser() {
+		
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		
+	}
+
+}
